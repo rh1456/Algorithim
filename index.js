0652e318aa9f2ec2385697d42d9898e45edea434
rh1456 <reidhanson2@gmail.com>
2020-03-10 03:43:20 +0000

    Runtime linearity

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-function harmLessRansomNote (noteText, magazineText) {
+// function harmLessRansomNote (noteText, magazineText) {
 
+// }
+
+// Big O can give us an idea of how long an algorithim will take to complete based on input size //
+
+function log(array) {
+  console.log(array[0]);
+  console.log(array[1]);
+} 
+
+log ([1,2,3,4]);
+log ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+//run time will be constant becasue as the input size increasees the number of opersations we before never changes//
+function logALl(array) {
+  for (var i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
 }
 
-// can give us an idea of how long an algorithim will take to complete based on input size //
+logAll ([1,2,3,4,5]);
+logAll ([1,2,3,4,5,6]);
+logAll ([1,2,3,4,5,6,7]);
