fe0f760257b23092fb0bda01c8948160b83f7847
rh1456 <reidhanson2@gmail.com>
2020-03-10 03:21:40 +0000

    passed 30 as param

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
- 
-
 
 function fizzBuzz(num) {
-for (var i = 1; 1 <= num; i++){
-  if (i % 15 === 0) console.log('FizzBuzz');
-  if (i % 3 === 0) console.log('Fizz');
-  else if (i % 5 === 0) console.log('Buzz');
-  else console.log(i);
+  for (var i = 1; i <= num; i++) {
+    if (i % 15 === 0) console.log('FizzBuzz');
+    else if (i % 3 === 0) console.log('Fizz');
+    else if (i % 5 === 0) console.log('Buzz');
+    else console.log(i);
   }
 }
-fizzBuzz(30); 
+ 
+fizzBuzz(30);
