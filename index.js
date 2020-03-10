35a91786e3890e89781a88471e036397505b4e6d
rh1456 <reidhanson2@gmail.com>
2020-03-10 03:45:20 +0000

    Big O notation notes

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ log ([1,2,3,4]);
 log ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
 //run time will be constant becasue as the input size increasees the number of opersations we before never changes//
+
+//linear runtime
+//Big O notation: "O (n)"
+
 function logALl(array) {
   for (var i = 0; i < array.length; i++) {
     console.log(array[i]);
